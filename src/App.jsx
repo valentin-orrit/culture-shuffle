7a93e215eb17e5ab73a241b5ec42c78ce0847d5d
valentin-orrit <vorrit@gmail.com>
2024-10-31 17:06:32 +0100

    added blobs

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,20 @@ function App() {
     return (
         <>
             <main>
-                <h1>culture shuffle</h1>
-                <p>Are you a true art nerd? Find out in 10 questions.</p>
+                <div className="blob-container">
+                    <div className="blob blob-top-right"></div>
+                    <div className="blob blob-bottom-left"></div>
+                </div>
+                <div className="home--title">
+                    <h1>culture shuffle</h1>
+                    <p>Are you a true art nerd? Find out in 10 questions.</p>
+                </div>
+                <div className="home--chose-challenge">
+                    <p>choose your challenge</p>
+                </div>
+                <button className="home--start-btn big-btn">
+                    start quizz!
+                </button>
             </main>
         </>
     )
