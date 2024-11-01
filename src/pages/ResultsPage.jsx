28e84fd7e29ce07bdafcb2f03c566b3f79d030fa
valentin-orrit <vorrit@gmail.com>
2024-11-01 17:26:21 +0100

    added button to start quiz again

--- src/pages/ResultsPage.jsx
+++ src/pages/ResultsPage.jsx
@@ -1,17 +1,25 @@
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import Blobs from '../components/Blobs'
 
 export default function ResultsPage() {
     const location = useLocation()
+    const navigate = useNavigate()
     const questions = location.state?.data
 
-    // const correctAnswers = questions.map((question) => question.correct_answer)
+    //const correctAnswers = questions.map((question) => question.correct_answer)
+
+    function newQuiz() {
+        navigate('/')
+    }
 
     return (
         <main>
             {console.log(questions)}
             <Blobs />
             <h1>Results</h1>
+            <button className="home--start-btn big-btn" onClick={newQuiz}>
+                Try again
+            </button>
         </main>
     )
 }
