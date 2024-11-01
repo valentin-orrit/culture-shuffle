b296a9546cf7c0f8b29e6a569bddfb6dfac906cc
valentin-orrit <vorrit@gmail.com>
2024-11-01 10:53:57 +0100

    added category buttons

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,26 +1,54 @@
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 import Blobs from '../components/Blobs'
 
 export default function HomePage() {
+    const [active, setActive] = useState(false)
+
+    // category list
+    const answers = [
+        { id: 1, text: 'music', category: 12 },
+        { id: 2, text: 'cinema', category: 11 },
+        { id: 3, text: 'fine arts', category: 25 },
+        { id: 4, text: 'literature', category: 10 },
+    ]
+
+    // Starting the quiz
     const navigate = useNavigate()
 
-    function startQuiz() {
+    function startQuiz(e) {
+        e.preventDefault()
         navigate('/quiz')
     }
 
     return (
         <main>
             <Blobs />
+
             <div className="home--title">
                 <h1>culture shuffle</h1>
                 <p>Are you a true art nerd? Find out in 10 questions.</p>
             </div>
-            <div className="home--chose-challenge">
-                <p>choose your challenge</p>
-            </div>
-            <button className="home--start-btn big-btn" onClick={startQuiz}>
-                start quiz!
-            </button>
+
+            <form className="home--form">
+                <div className="home--chose-challenge">
+                    <p>choose your challenge</p>
+                </div>
+                <div className="home--chose-btns">
+                    {answers.map((item) => (
+                        <button key={item.id} className="small-btn">
+                            {item.text}
+                        </button>
+                    ))}
+                </div>
+                <button
+                    className="home--start-btn big-btn"
+                    onClick={startQuiz}
+                    type="submit"
+                >
+                    start quiz!
+                </button>
+            </form>
         </main>
     )
 }
